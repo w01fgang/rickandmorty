359aa77f8411e74db03d6ad2cf48285f416f904c
w01fgang <sumin@unix-center.ru>
2020-11-04 00:23:09 +0100

    :label: add type definitions

---
+++ flow-typed/npm/next-redux-wrapper_vx.x.x.js
@@ -0,0 +1,7 @@
+// flow-typed signature: 4287cb230af35ec44d141a0f4c03b8ee
+// flow-typed version: <<STUB>>/next-redux-wrapper_v^6.0.2/flow_v0.137.0
+
+declare module 'next-redux-wrapper' {
+  declare export var HYDRATE: "HYDRATE";
+  declare export default any;
+}
