8b4ba326f1b758d8819bc70d2c7c73e4635869ca
w01fgang <sumin@unix-center.ru>
2020-11-05 18:44:38 +0100

    :heavy_plus_sign: add react-responsive

---
+++ flow-typed/npm/react-responsive_vx.x.x.js
@@ -0,0 +1,6 @@
+// flow-typed signature: d75e557c90e14619ca0b7c5d94bf3b7a
+// flow-typed version: module/react-responsive_v8.1.0/flow_v0.137.0
+
+declare module 'react-responsive' {
+  declare export function useMediaQuery({ query: string }): boolean
+}
